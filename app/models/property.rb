3b1c9e91e3cea87be61543ee4851bb77edd25099
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-06-24 18:26:54 -0300

    ajustei o model property com validacao de presenca de colunas na tabela

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,7 @@
 class Property < ApplicationRecord
+  # Validação para garantir que as colunas na tabela imoveis estejam presentes
+  validates :title, presence: true, uniqueness: true
+  validates :size, presence: true
+  validates :bedroom_count, presence: true
+  validates :bathroom_count, presence: true
 end
